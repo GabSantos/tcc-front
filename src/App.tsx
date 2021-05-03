05994b9f2df132724366ee034a29e43c6ae6c0e1
Gabriel Santos <gabrogolt@gmail.com>
2021-05-03 13:45:34 -0300

    expo 2

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ChakraProvider, CSSReset } from '@chakra-ui/react';
+import { Text } from 'react-native';
+
+const Route: React.FC = () => {
+  return <Text>Open up App.tsx to start working on your app!</Text>;
+};
+export default Route;
